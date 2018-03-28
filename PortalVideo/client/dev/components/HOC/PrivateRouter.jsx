a09e1f4c2c17b9c4814fed93afb977b5653c3c82
DracotMolver <dracotm25@gmail.com>
2018-03-27 21:33:15 -0300

    removed parenthesis

--- PortalVideo/client/dev/components/HOC/PrivateRouter.jsx
+++ PortalVideo/client/dev/components/HOC/PrivateRouter.jsx
@@ -13,12 +13,9 @@ const isAuthenticated = () => !!JSON.parse(sessionStorage.getItem('userData'));
 
 const PrivateRouter = ({ component: Component, ...rest }) =>
     isAuthenticated()
-        ? (
-            <Route {...rest} render={(props) => (
-                <Component {...props} />)
-            } />
-        ) : (
-            <Redirect to="/" />
-        );
+        ? <Route {...rest} render={props => (
+            <Component {...props} />)
+        } />
+        : <Redirect to="/" />;
 
 export default PrivateRouter;
