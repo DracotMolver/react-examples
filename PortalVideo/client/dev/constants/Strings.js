c73d23fb3f6532c4529cd4c977a4644a914eaa1c
DracotMolver <dracotm25@gmail.com>
2018-03-30 14:00:06 -0300

    Added common strings as contstants

---
+++ PortalVideo/client/dev/constants/Strings.js
@@ -0,0 +1,38 @@
+/**
+ * @author Diego Alberto Molina Vera
+ * @copyright 2017 - 2018
+ */
+
+// Home
+export const GREETINGS = 'Hi!';
+
+// Form login
+export const USER_AND_PASS_ERROR = 'The Username and the Password field can\'t be empty';
+export const FORM_LOGIN_TITLE = 'Video portal';
+export const INPUT_USER_LABEL = 'User name';
+export const INPUT_PASS_LABEL = 'Password';
+export const FORM_LOGIN_LABEL = 'Log in';
+export const FORM_LOGOUT_LABEL = 'Log out';
+
+// Video Card
+export const BACK_BUTTON = 'Back to the list';
+export const RATE_BUTTON = 'Rate!';
+
+// Video Full Screen
+export const PLAY_BUTTON = 'Play!';
+
+// Video Rate popup
+export const CLOSE_BUTTON = 'Close';
+export const POPUP_TITLE_INITIAL = 'How much did you enjoy the video?';
+export const POPUP_BUTTON = 'Done!';
+export const POPUP_TITLE_SUCCESS = 'Thanks for rating the video! :)';
+
+// Video list container
+// Video single container
+export const LOAD_VIDEOS_ERROR = 'There was an error trying to load the videos. Please, try later';
+export const LOAD_VIDEOS_BUTTON = 'Show me more videos!';
+
+// Errors
+export const DEFAULT_ERROR_MESSAGE = 'There was an error. Please, try later';
+export const TYPE_ERROR = 'error';
+export const TYPE_WARNING = 'warning';
