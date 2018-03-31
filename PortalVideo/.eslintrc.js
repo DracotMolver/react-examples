76831c7db747d6c949c29b243b568ad07cdf8474
DracotMolver <dracotm25@gmail.com>
2018-03-31 12:37:02 -0300

    replaced eslint

--- PortalVideo/.eslintrc.js
+++ PortalVideo/.eslintrc.js
@@ -1,3 +1,32 @@
 module.exports = {
-    "extends": "airbnb"
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/jsx-uses-vars": "error",
+        "indent": [
+            "error",
+            4
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    },
 };
