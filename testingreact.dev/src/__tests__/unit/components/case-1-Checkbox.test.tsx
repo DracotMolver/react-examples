d70da0d69cce875358740c35f827aa2c53d3135d
DracotMolver <dracotm25@gmail.com>
2020-10-07 21:52:55 -0300

    mater class by Kati Frantz

---
+++ testingreact.dev/src/__tests__/unit/components/case-1-Checkbox.test.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { axe } from 'jest-axe'
+import { render, fireEvent } from '@testing-library/react'
+
+import Checkbox from '../../../components/Checkbox'
+
+/**
+ * This checkbox component renders a checkbox with a label.
+ * Since we customized the default checkbox, we want to
+ * make sure it still works as a regular checkbox
+ * should.
+ */
+describe('The <Checkbox /> component', () => {
+  const defaultCheckboxProps = {
+    label: 'TEST_CHECKBOX_LABEL',
+    id: 'TEST_CHECKBOX_ID',
+    background: '#000',
+    checkMarkBackground: '#fff',
+    onChange: jest.fn(),
+    checked: false,
+  }
+
+  const setupCheckbox = () => render(<Checkbox {...defaultCheckboxProps} />)
+
+  it('Should render the label and checkbox the user will see', () => {
+    const { asFragment } = setupCheckbox()
+
+    expect(asFragment).toMatchSnapshot()
+  })
+
+  it('Should make the checkbox accessible by setting the id and htmlFor attributes on label and checkbox', () => {
+    const { getByLabelText } = setupCheckbox();
+
+    const label = getByLabelText('TEST_CHECKBOX_LABEL')
+
+    expect(label).toBeInTheDocument()
+  })
+
+  it('❌ Should call the onChange handler when it is provided', () => {})
+
+  it('❌ Should change state correctly when clicked (checked and unchecked)', () => {})
+
+  it('❌ should not fail any accessibility tests', async () => {})
+})
